acfb0273ef35bdf5ae2863d24e7a5b7c1ff05b50
SphericalKat <amolele@gmail.com>
2021-07-04 22:07:28 +0530

    feat(di): add db and dao to dependency graph
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- app/src/main/java/at/sphericalk/gidget/data/db/AppDb.kt
+++ app/src/main/java/at/sphericalk/gidget/data/db/AppDb.kt
@@ -1,9 +1,10 @@
 package at.sphericalk.gidget.data.db
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import at.sphericalk.gidget.model.Event
 
 @Database(entities = [Event::class], version = 1)
-abstract class AppDb {
+abstract class AppDb : RoomDatabase() {
     abstract fun eventDao(): EventDao
 }
---
+++ app/src/main/java/at/sphericalk/gidget/di/DbModule.kt
@@ -0,0 +1,24 @@
+package at.sphericalk.gidget.di
+
+import android.content.Context
+import androidx.room.Room
+import at.sphericalk.gidget.data.db.AppDb
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DbModule {
+    @Provides
+    @Singleton
+    fun providesAppDb(@ApplicationContext context: Context) =
+        Room.databaseBuilder(context, AppDb::class.java, "gimmick-db").build()
+
+    @Provides
+    @Singleton
+    fun providesEventDao(db: AppDb) = db.eventDao()
+}
